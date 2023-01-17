2878db76b78c47867d19d15bfe599c9d8d057a9c
Artem <grekartemvl@mail.ru>
2023-01-17 14:05:14 +0500

    Add Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,29 @@
+﻿// 28. Напишите программу, которая принимает на вход число N и
+// выдает произведение чисел от 1 до N. 4 -> 24. 5 -> 120.
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+int getMultiply(int start, int end)
+{
+    int mult = 1;
+    for (int i = start; i <= end; i++)
+    {
+        mult *= i; //mult = mult * 1
+    }
+    return mult;
+}
+void showData(string messageStart, int data)
+{
+    Console.Write(messageStart);
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write(data);
+    Console.ResetColor();
+}
+int end = getUserData("Введите число N для получения произведения");
+int mult = getMultiply(1, end);
+showData($"Произведение чисел от 1 до {end} = ", mult);
