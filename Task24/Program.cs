4a54501553e87e6024629255ea4ea2779c4e6c43
Artem <grekartemvl@mail.ru>
2023-01-17 13:13:18 +0500

    Add Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,31 @@
+﻿// 24. Напишите программу, которая принимает на вход число A 
+// и выдает сумму чисел от 1 до А
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int getSumOfRange(int start, int end)
+{
+    int sum = 0;
+    for (int i = start; i <= end; i++)
+    {
+        sum += i; //sum = sum + 1
+    }
+    return sum;
+}
+void showData(string messageStart, int data)
+{
+    Console.Write(messageStart);
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write(data);
+    Console.ResetColor();
+}
+int end = getUserData("Введите число A для получения суммы");
+int sum = getSumOfRange(1, end);
+showData($"Сумма чисел от 1 до {end} = ", sum);
